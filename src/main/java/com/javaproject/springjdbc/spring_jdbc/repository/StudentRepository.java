bf7404125bef27d4b29b29cdae317c87da307bf0
Vedant Karale <vedantkarale271@gmail.com>
2025-04-06 22:56:22 +0530

    jdbctemplate schema.sql data.sql h2 database

--- src/main/java/com/javaproject/springjdbc/spring_jdbc/repository/StudentRepository.java
+++ src/main/java/com/javaproject/springjdbc/spring_jdbc/repository/StudentRepository.java
@@ -1,6 +1,8 @@
 package com.javaproject.springjdbc.spring_jdbc.repository;
 
 import com.javaproject.springjdbc.spring_jdbc.model.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -9,8 +11,21 @@ import java.util.List;
 
 @Repository
 public class StudentRepository {
+    private JdbcTemplate jdbcTemplate;
+
+    public JdbcTemplate getJdbcTemplate() {
+        return jdbcTemplate;
+    }
+
+    @Autowired
+    public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     public void save(Student student){
-        System.out.println("Student Added Successfully");
+        String sql = "insert into student(rollno, name, marks) values(?,?,?)";
+        int rows = jdbcTemplate.update(sql, student.getRollNo(), student.getName(), student.getMarks());
+        System.out.println(rows+" affected");
     }
 
     public List<Student> findAll(){
---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into student(rollno, name, marks) values(102, 'Rohit', 99);
+insert into student(rollno, name, marks) values(103, 'Mohit', 98);
